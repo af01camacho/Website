074df58ec0188597015e5f8de9f60e8dd1bd0ffe
Andres Camacho <99270915+af01camacho@users.noreply.github.com>
2023-07-23 16:03:37 -0500

    fix

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,18 +1,20 @@
 function Cards() {
   return (
     <>
-      <div className="flex flex-row absolute top-[650px] gap-5 p-5">
-        <div className="bg-white rounded-sm w-[300px] h-[300px] shadow-2xl justify-center flex py-2">
-          <h2>Design Web</h2>
-        </div>
-        <div className="bg-white rounded-sm w-[300px] h-[300px] shadow-2xl justify-center flex py-2">
-          <h2>Design Web</h2>
-        </div>
-        <div className="bg-white rounded-sm w-[300px] h-[300px] shadow-2xl justify-center flex py-2">
-          <h2>Design Web</h2>
-        </div>
-        <div className="bg-white rounded-sm w-[300px] h-[300px] shadow-2xl justify-center flex py-2 ">
-          <h2>Design Web</h2>
+      <div className="flex justify-center">
+        <div className="flex flex-wrap absolute top-[650px] gap-3 ">
+          <div className="bg-white rounded-sm w-[300px] h-[300px] shadow-2xl justify-center flex py-2">
+            <h2>Design Web</h2>
+          </div>
+          <div className="bg-white rounded-sm w-[300px] h-[300px] shadow-2xl justify-center flex py-2">
+            <h2>Design Web</h2>
+          </div>
+          <div className="bg-white rounded-sm w-[300px] h-[300px] shadow-2xl justify-center flex py-2">
+            <h2>Design Web</h2>
+          </div>
+          <div className="bg-white rounded-sm w-[300px] h-[300px] shadow-2xl justify-center flex py-2 ">
+            <h2>Design Web</h2>
+          </div>
         </div>
       </div>
     </>
--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -3,13 +3,15 @@ import Cards from "../cards/cards";
 
 function Hero() {
   return (
-    <div className="flex h-[700px] items-center bg-[#0b0d4d] text-white">
+    <div className="flex flex-wrap h-[700px] items-center bg-[#0b0d4d] text-white">
       <div className="w-[50%] justify-center items-center flex flex-wrap">
-        <h1 className="text-4xl  font-extrabold">HACEMOS TUS IDEAS REALIDAD</h1>
+        <h1 className="text-4xl flex flex-wrap text-center font-extrabold">
+          HACEMOS TUS IDEAS REALIDAD
+        </h1>
         <h2 className="text-2xl font-bold">
           Encuntra asesoria en uno de nuestros experto
         </h2>
-        <div className="flex flex-col w-4/5 gap-y-4 py-5">
+        <div className="flex flex-col w-4/5 gap-y-5 ">
           <input
             className="p-2"
             placeholder="Nombre"
