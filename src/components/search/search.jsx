3d8aba1ce53c940ce3271c499e1db012f57331f3
Andres Camacho <99270915+af01camacho@users.noreply.github.com>
2023-07-25 08:43:59 -0500

    Update

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import { QueryDomains } from '../../APIS/dominios';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { QueryDomains } from "../../APIS/dominios";
 
 const Search = ({ onSearch }) => {
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
 
   const handleSearchInput = (e) => {
     setSearchQuery(e.target.value);
   };
 
   const handleSearchButtonClick = () => {
-    if (searchQuery !== '') {
+    if (searchQuery !== "") {
       QueryDomains.searchDomains(searchQuery)
         .then((data) => {
-          console.log('API Response:', data);
+          console.log("API Response:", data);
           const results = data.map((domain) => ({
             domain: domain.domainName,
-            available: domain.domainAvailability === 'AVAILABLE',
+            available: domain.domainAvailability === "AVAILABLE",
           }));
           onSearch(results);
         })
@@ -30,18 +30,18 @@ const Search = ({ onSearch }) => {
   };
 
   return (
-    <div className="relative m-2">
-      <div className="flex items-center w-[400px] justify-araound border-4 rounded-lg overflow-hidden">
+    <div className="relative m-2 font-primary-font">
+      <div className="flex items-center  w-[500px] outline-none border-none bg-white p-2 justify-araound border-4 rounded-lg overflow-hidden">
         <input
           type="text"
           onChange={handleSearchInput}
           value={searchQuery}
-          placeholder="Buscar dominio"
-          className="border-0 p-2 w-full h-[50px] rounded-md focus:outline-none"
+          placeholder="Buscar un dominio disponible"
+          className="border-0 p-2 px-16 w-full h-[50px] placeholder:text-center rounded-md focus:outline-none"
         />
         <button
           onClick={handleSearchButtonClick}
-          className="bg-[#ff6b00] text-white px-8 py-2 rounded-r-lg"
+          className="bg-[#ff6b00] text-white px-9 py-3 rounded-lg w-1/4"
         >
           <FontAwesomeIcon icon={faSearch} />
         </button>
--- src/secciones/seccionBuscar/seccionBuscar.jsx
+++ src/secciones/seccionBuscar/seccionBuscar.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import Search from '../../components/search/search';
+import { useState } from "react";
+import Search from "../../components/search/search";
 
 const SeccionBuscar = () => {
   const [searchResults, setSearchResults] = useState([]);
@@ -9,12 +9,15 @@ const SeccionBuscar = () => {
   };
 
   return (
-    <div className="py-10 px-4 md:px-8 absolute bottom-0 left-0 right-0 top-[1070px]">
-      <div className="flex flex-col md:flex-row items-center justify-between mb-4 pl-10 pr-10">
-        <div className="md:w-1/2 break-words">
-          <h2 className="text-3xl font-semibold mb-2">Registra tu dominio Aquí hoy</h2>
-          <p className="text-xl text-gray-600 mb-4">
-            En el registro de tu primer dominio .com o .org contratando cualquiera de nuestros planes
+    <div className="py-10 px-4 md:px-8  absolute bottom-0 left-0 right-0 md:top-[1100px] bg-primary-color h-fit  lg:top-[1070px]">
+      <div className="flex lg:flex-row md:flex-col items-center justify-between mb-4 pl-10 pr-10">
+        <div className="md:w-1/2 break-words text-center">
+          <h2 className="text-3xl font-extrabold uppercase text-white mb-2 font-primary-font">
+            Registra tu dominio Aquí hoy
+          </h2>
+          <p className="text-xl text-gray-400 mb-4 font-primary-font">
+            En el registro de tu primer dominio .com o .org contratando
+            cualquiera de nuestros planes
           </p>
         </div>
         <div className="md:w-1/2 flex justify-end items-center">
@@ -23,13 +26,11 @@ const SeccionBuscar = () => {
       </div>
       <div className="grid gap-4 grid-cols-2 md:grid-cols-4">
         {searchResults.map((result, index) => (
-          <div
-            key={index}
-            className="border p-4 rounded-md shadow-md"
-          >
+          <div key={index} className="border p-4 rounded-md shadow-md">
             <p className="font-semibold mb-2">Dominio: {result.domain}</p>
             <p className="text-sm">
-              Disponibilidad: {result.available ? 'Disponible' : 'No disponible'}
+              Disponibilidad:{" "}
+              {result.available ? "Disponible" : "No disponible"}
             </p>
           </div>
         ))}
