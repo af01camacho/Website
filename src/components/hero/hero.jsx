82d50d3c1b55dc35bbcf8e509efb6d453219e280
Andres Camacho <99270915+af01camacho@users.noreply.github.com>
2023-07-25 08:27:49 -0500

    mobile

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -40,10 +40,34 @@ export function HeroDesktop() {
 }
 export function HeroMobile() {
   return (
-    <>
-      <div className="md:hidden  flex bg-[#0b0d4d] text-white p-2 text-center">
+    <div className="">
+      <div className="flex-col flex-wrap items-center gap-5 md:hidden py-4 flex bg-[#0b0d4d] text-white p-2 text-center">
         <TypeWriter />
+
+        <img className="w-3/4 " src={imghero} alt="" />
+
+        <div className="flex flex-col items-center gap-5">
+          <input
+            className="w-[400px] p-3 outline-0 text-black"
+            placeholder="Nombre"
+            type="text"
+            name="name"
+            id=""
+          />
+          <input
+            className="w-[400px] p-3 outline-0 text-black"
+            placeholder="telefono"
+            type="number"
+            name=""
+            id=""
+          />
+          <h4>*No reveles informacion de tarjetas de creditos</h4>
+
+          <button className="bg-second-color p-3 rounded-sm w-3/5">
+            Registrarse
+          </button>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
