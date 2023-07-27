41dc283ca21db7fb2e0a9ed117a0053532937608
Andres Camacho <99270915+af01camacho@users.noreply.github.com>
2023-07-27 16:50:36 -0500

    Update cards-price.jsx

--- src/secciones/cards-price/cards-price.jsx
+++ src/secciones/cards-price/cards-price.jsx
@@ -18,22 +18,22 @@ function CardsPrice() {
       <h2 className="text-4xl font-extrabold font-primary-font text-center py-5">
         Planes de hosting web
       </h2>
-      <div className="grid grid-cols-3 place-content-center place-items-center">
-        <div className="relative h-80 w-[300px] rounded-md bg-white shadow-2xl">
+      <div className="grid md:grid-cols-3 lg:grid-cols-3 grid-cols-1 place-content-center gap-5 place-items-center">
+        <div className="relative h-80 md:w-[300px] lg:w-[300px] w-3/4 rounded-md bg-white shadow-2xl">
           <div className="ribbon absolute h-40 w-40 -top-2 -left-2  overflow-hidden before:absolute before:top-0 before:right-0 before:border-4 before:border-blue-500  after:absolute after:bottom-0 after:left-0 after:border-4 after:border-blue-500">
             <div className="absolute -left-14 top-10 w-60 bg-gradient-to-br from-blue-600 via-blue-400 to-blue-500 py-2.5 text-center text-white shadow-md -rotate-45">
               40% descuento
             </div>
           </div>
         </div>
-        <div className="relative h-80 w-[300px] rounded-md bg-white shadow-2xl">
+        <div className="relative h-80 md:w-[300px] lg:w-[300px] w-3/4 rounded-md bg-white shadow-2xl">
           <div className="ribbon absolute h-40 w-40 -top-2 -left-2  overflow-hidden before:absolute before:top-0 before:right-0 before:border-4 before:border-red-500  after:absolute after:bottom-0 after:left-0 after:border-4 after:border-red-500">
             <div className="absolute -left-14 top-10 w-60 bg-gradient-to-br from-red-600 via-red-400 to-red-500 py-2.5 text-center text-white shadow-md -rotate-45">
               40% descuento
             </div>
           </div>
         </div>
-        <div className="relative h-80 w-[300px] rounded-md bg-white shadow-2xl">
+        <div className="relative h-80 md:w-[300px] lg:w-[300px] w-3/4 rounded-md bg-white shadow-2xl">
           <div className="ribbon absolute h-40 w-40 -top-2 -left-2  overflow-hidden before:absolute before:top-0 before:right-0 before:border-4 before:border-blue-500  after:absolute after:bottom-0 after:left-0 after:border-4 after:border-blue-500">
             <div className="absolute -left-14 top-10 w-60 bg-gradient-to-br from-blue-600 via-blue-400 to-blue-500 py-2.5 text-center text-white shadow-md -rotate-45">
               40% descuento
