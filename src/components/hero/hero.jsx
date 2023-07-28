c8e16a799d7090a8665cde6910a228a7184c6060
Andres Camacho <99270915+af01camacho@users.noreply.github.com>
2023-07-27 21:45:47 -0500

    Update

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -7,25 +7,27 @@ export function Hero() {
         {/* Card 1  */}
         <div className="text-white flex items-center lg:order-1 md:order-1 order-2 justify-center flex-col">
           <TypeWriter />
-          <h2>Quedate seguro en manos de nuestros expertos.!</h2>
+          <h2 className="text-sm font-semibold text-center">
+            Quedate seguro en manos de nuestros expertos.!
+          </h2>
           <div className="flex flex-col gap-2 p-2 items-center">
             <input
-              className=" w-[400px]  p-2 text-black outline-none"
+              className=" w-[290px] lg:w-[400px] sm:w-[400px] md:w-[400px]  p-2 text-black outline-none"
               type="text"
               placeholder="Nombre"
             />
             <input
-              className="  w-[400px] text-black p-2 outline-none"
+              className=" w-[290px] lg:w-[400px] sm:w-[400px] md:w-[400px]  text-black p-2 outline-none"
               placeholder="Telefono"
               type="tel"
               name=""
               id=""
             />
-            <span>
-              * No reveles informacion extra como tarjetas de credito *
+            <span className="text-center text-sm">
+              * No reveles informacion de tarjetas de credito
             </span>
             <input
-              className=" w-[400px] bg-second-color font-primary-font text-l font-bold p-2"
+              className=" w-[290px] lg:w-[400px] md:w-[400px]  bg-second-color font-primary-font text-l font-bold p-2"
               type="submit"
               value="Enviar"
             />
@@ -33,8 +35,8 @@ export function Hero() {
         </div>
 
         {/* Card 2 */}
-        <div className="w-[450px] justify-center order-1 lg:order-2 md:order-2 flex items-center">
-          <img src={imghero} alt="" />
+        <div className="w-fit justify-center order-1 lg:order-2 md:order-2 flex items-center">
+          <img className="" src={imghero} alt="" />
         </div>
       </div>
     </>
