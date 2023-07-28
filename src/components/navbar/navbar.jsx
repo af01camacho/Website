628cb8c2e8fea8f6c2ad65224d750ee01ea4414f
Gabriel Martínez <martinegabriel47@gmail.com>
2023-07-28 10:31:49 -0500

    Update navbar.jsx

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,8 +2,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faUser } from "@fortawesome/free-solid-svg-icons";
 
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export function NavbarDesktop() {
+  const menu = [
+    {link: "Aplicaciones", route: '/'},
+    {link: "Servicios", route: '/services'},
+    {link: "Contactanos", route: '/contacto'},
+    {link: "Portafolio", route: '/portafolio'}
+  ]
+
   return (
     <>
       <nav className="md:hidden sm:hidden bg-[#000248f2] lg:flex hidden  items-center justify-around h-16">
@@ -14,24 +22,13 @@ export function NavbarDesktop() {
         </div>
         {/* Menu */}
         <ul className="text-white font-primary-font uppercase font-bold  gap-5 flex">
-          <Link
-            className="border-b-2 border-spacing-y-10 text-second-color"
-            to="/"
-          >
-            Aplicaciones
-          </Link>
-          <Link
-            className=" hover:text-second-color  active:border-b-2"
-            to="/services"
-          >
-            Servicios
-          </Link>
-          <Link className=" hover:text-second-color  active:border-b-2">
-            Contactanos
-          </Link>
-          <Link className=" hover:text-second-color  active:border-b-2">
-            Portafolio
-          </Link>
+          {menu.map((item, index) => {
+            return(
+              <li key={index} className="border-spacing-y-10 text-second-color">
+                <Link to={item.route}>{item.link}</Link>
+              </li>
+            )
+          })}
         </ul>
         <div>
           <button className="bg-second-color px-4 py-2 rounded-full text-white text-m">
@@ -44,29 +41,50 @@ export function NavbarDesktop() {
 }
 
 export function NavbarMobile() {
+  const [active, setActive] = useState(false);
+
+  const showMenu = () => {
+    setActive(!active);
+  };
+
+  const menu = [
+    { link: "Aplicaciones", route: "/" },
+    { link: "Servicios", route: "/services" },
+    { link: "Contactanos", route: "/contacto" },
+    { link: "Portafolio", route: "/portafolio" },
+  ];
+
   return (
     <>
-      <div className="bg-[#0d0f51] lg:hidden flex justify-evenly h-14  items-center">
+      <div className="bg-[#0d0f51] lg:hidden flex justify-evenly h-14 items-center">
         {/* Menu */}
-        <div className=" p-1 rounded-s">
-          <FontAwesomeIcon
-            icon={faBars}
-            size="xl"
-            style={{ color: "#f2f2f2" }}
-          />
+        <div className="p-1 rounded-s cursor-pointer" onClick={showMenu}>
+          <FontAwesomeIcon icon={faBars} size="xl" style={{ color: "#f2f2f2" }} />
         </div>
 
-        {/* Logo */}
         <div className="flex uppercase text-primary-color font-primary-font font-extrabold text-2xl justify-center">
           <h2 className="text-second-color">Master</h2>
           <h2 className="text-white">Web</h2>
         </div>
+
         <div>
-          <button className="bg-second-color p-1 rounded-sm text-white font-primary-font ">
+          <button className="bg-second-color p-1 rounded-sm text-white font-primary-font">
             Contactarnos
           </button>
         </div>
       </div>
+
+      <ul
+        className={`${
+          active ? "block" : "hidden"
+        } bg-[#0d0f51] lg:hidden flex flex-col gap-5 text-white font-primary-font uppercase font-bold items-center py-2`}
+      >
+        {menu.map((item, index) => (
+          <li key={index} className="border-spacing-y-10 text-second-color">
+            <Link to={item.route}>{item.link}</Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
