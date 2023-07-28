3ec71693ef39eacae613fe36a1b377ed3826783c
Andres Camacho <99270915+af01camacho@users.noreply.github.com>
2023-07-27 21:38:58 -0500

    Update hero.jsx

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -5,7 +5,7 @@ export function Hero() {
     <>
       <div className="grid grid-cols-1 lg:grid-cols-2 place-items-center   md:grid-cols-2 bg-primary-color ">
         {/* Card 1  */}
-        <div className="text-white flex items-center order-2 justify-center flex-col">
+        <div className="text-white flex items-center lg:order-1 md:order-1 order-2 justify-center flex-col">
           <TypeWriter />
           <h2>Quedate seguro en manos de nuestros expertos.!</h2>
           <div className="flex flex-col gap-2 p-2 items-center">
@@ -33,7 +33,7 @@ export function Hero() {
         </div>
 
         {/* Card 2 */}
-        <div className="w-[450px] justify-center order-1 flex items-center">
+        <div className="w-[450px] justify-center order-1 lg:order-2 md:order-2 flex items-center">
           <img src={imghero} alt="" />
         </div>
       </div>
