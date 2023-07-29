1bd98f484f3aec640929ff1a0727caa6145e68bd
Andres Camacho <99270915+af01camacho@users.noreply.github.com>
2023-07-29 11:03:38 -0500

    Rutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/home";
-import { useState } from "react";
+import Services from "./pages/services/services";
 
 function App({ toogleDarkMode }) {
-  const [DarkMode, setDarkMode] = useState(false);
-
-  function toogleDarkMode() {
-    if (DarkMode == false) {
-      document.querySelector("html").classList.toggle("dark");
-    }
-  }
-
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home DarkMode={toogleDarkMode} />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
       </Routes>
     </>
   );
