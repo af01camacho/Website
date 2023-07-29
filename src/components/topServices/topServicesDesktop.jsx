e6b5f968ccbcffe8449a69dd337266cae2648f5f
Andres Camacho <99270915+af01camacho@users.noreply.github.com>
2023-07-29 18:08:23 -0500

    Update topServicesDesktop.jsx

--- src/components/topServices/topServicesDesktop.jsx
+++ src/components/topServices/topServicesDesktop.jsx
@@ -52,15 +52,15 @@ const TopServicesDesktop = () => {
         {servicios.map((servicio, index) => (
           <div
             key={index}
-            className="flex flex-col bg-white items-center h-[270px] p-4 shadow-lg rounded-lg"
+            className="flex flex-col dark:bg-gray-700  bg-white items-center h-[270px] p-4 shadow-lg rounded-lg"
           >
             <span className="text-4xl text-primary-color mb-4 bg-[#E1EFFE] w-14 h-14 flex justify-center items-center rounded-lg">
               <FontAwesomeIcon icon={servicio.icon} color="#1A56DB" />
             </span>
-            <h2 className="text-gray-900 text-lg font-semibold leading-snug mb-2">
+            <h2 className="dark:text-white text-gray-900 text-lg font-semibold leading-snug mb-2">
               {servicio.title}
             </h2>
-            <p className="text-gray-500 text-base font-normal leading-normal text-center">
+            <p className="dark:text-white text-gray-500 text-base font-normal leading-normal text-center">
               {servicio.info}
             </p>
           </div>
