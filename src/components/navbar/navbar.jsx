1706e5df5fe387aa316a8600dab5ece4046073e4
Andres Camacho <99270915+af01camacho@users.noreply.github.com>
2023-07-29 10:45:08 -0500

    update darkmode

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -9,7 +9,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function Navbar() {
   return (
     <>
-      <nav className="font-primary-font">
+      <nav className="font-primary-font dark:bg-primary-color-dm dark:text-white">
         <div className="flex justify-between px-16">
           <div className="flex items-center gap-4 text-sm">
             <FontAwesomeIcon icon={faPhone} /> <span>3138024690</span>
@@ -17,15 +17,23 @@ function Navbar() {
             <span>soporte@techmastersweb.com</span>
           </div>
           <div className="flex p-2 justify-end gap-1 ">
-            <select className="outline-none cursor-pointer" name="" id="">
+            <select
+              className="outline-none cursor-pointer dark:bg-primary-color-dm"
+              name=""
+              id=""
+            >
               <option value="English">English</option>
               <option value="Spanish">Spanish</option>
             </select>
             <div className="flex items-center justify-center">
-              <hr className="  w-[20px] rotate-90" />
+              <hr className="  w-[20px] rotate-90 " />
             </div>
             <div>
-              <select className="outline-none cursor-pointer" name="" id="">
+              <select
+                className="outline-none cursor-pointer dark:bg-primary-color-dm"
+                name=""
+                id=""
+              >
                 <option value="USD">USD</option>
                 <option value="COL">COL</option>
               </select>
@@ -52,12 +60,12 @@ function Navbar() {
             <Link>Portafolio</Link>
           </ul>
           <div className="flex items-center justify-center">
-            <hr className="  w-[30px] rotate-90" />
+            <hr className="  w-[30px] rotate-90 " />
           </div>
-          <div className="border border-gray-200 p-2 rounded-md">
+          <div className="border dark:bg-[#4B5563] border-none p-2 pl-4 rounded-md">
             <FontAwesomeIcon icon={faMagnifyingGlass} />
             <input
-              className=" px-4 w-80 outline-none"
+              className=" px-4 w-80 outline-none dark:bg-[#4B5563]"
               placeholder="Search"
               type="search"
               name=""
