f82d9da3a27a0d09c79fca7b77ac408063e81d85
Andres Camacho <99270915+af01camacho@users.noreply.github.com>
2023-07-29 13:29:48 -0500

    Update navbarmobile.jsx

--- src/components/navbar/navbarmobile.jsx
+++ src/components/navbar/navbarmobile.jsx
@@ -1,10 +1,10 @@
 import { faMoon, faUser, faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import { Link } from "react-router-dom";
 function NavbarMobile() {
   return (
     <>
-      <nav className=" flex p-3   items-center justify-between">
+      <nav className=" flex h-full overflow-hidden p-2  items-center justify-between">
         <h2 className="font-semibold text-lg">TechMastersWeb</h2>
         <div className="flex items-center justify-center gap-4">
           <FontAwesomeIcon size="lg" icon={faMoon} />
@@ -14,6 +14,12 @@ function NavbarMobile() {
           <FontAwesomeIcon icon={faBars} />
         </div>
       </nav>
+      <div className="flex flex-col overflow-hidden float-right bg-btn-color w-52 h-[92.8vh] items-center  text-black ">
+        <Link className="">Home</Link>
+        <Link>Services</Link>
+        <Link>About</Link>
+        <Link>Portafolio</Link>
+      </div>
     </>
   );
 }
