2dc3bbba5003b26355d38bb0bdf947bf61cd1836
Andres Camacho <99270915+af01camacho@users.noreply.github.com>
2023-07-28 23:05:55 -0500

    Remaster

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-color-dm": "var(--primary-color-dm)",
+        "btn-color": "var(--btn-color)",
+        "span-color": "var(--span-color)",
+      },
+      fontFamily: {
+        "primary-font": "var(--primary-font)",
+        "second-font": "var(--second-font)",
+      },
+    },
+  },
+  plugins: [],
+};
