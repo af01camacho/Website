45c25dedafa53a849bf66c800acbf91071844ed4
Andres Camacho <99270915+af01camacho@users.noreply.github.com>
2023-07-29 14:39:42 -0500

    update

---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,43 @@
+export function HeroDesktop() {
+  return <>Desktop</>;
+}
+
+export function HeroTablet() {
+  return <>Tablet</>;
+}
+
+export function HeroMobile() {
+  return (
+    <>
+      <div className="font-primary-font p-2">
+        <div>
+          <h2 className="text-4xl font-extrabold">
+            Diseñamos por ti tu proxima gran idea de trabajo
+          </h2>
+          <span className="font-light">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga
+            pariatur voluptatibus corporis non velit assumenda perferendis,
+            reiciendis mollitia? Atque vitae quidem blanditiis. Tenetur
+            repudiandae adipisci corporis distinctio inventore, numquam
+            voluptatibus.
+          </span>
+        </div>
+        <div className="flex flex-col items-center text-center gap-4">
+          <input
+            className="border border-gray-200 rounded-md p-2 w-3/4"
+            placeholder="Coloca tu email"
+            type="email"
+            name=""
+            id=""
+          />
+          <input
+            className="bg-btn-color text-white font-semibold rounded-md p-2 w-3/4"
+            type="submit"
+            value="Enviar"
+          />
+          <span>* No reveles informacion de tarjetas de creditos</span>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,18 @@
+import { HeroDesktop, HeroMobile } from "../../components/hero/hero";
 import Navbar from "../../components/navbar/navbar";
 
 function Home({ toogleDarkMode }) {
   return (
     <>
       <Navbar />
+      {/* Design Mobile */}
+      <div className="contents md:hidden lg:hidden sm:hidden xl:hidden">
+        <HeroMobile />
+      </div>
+      {/* Design Desktop */}
+      <div className="hidden lg:contents xl:contents">
+        <HeroDesktop />
+      </div>
     </>
   );
 }
