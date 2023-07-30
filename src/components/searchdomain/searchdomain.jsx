0ee325e0c71bf2468410b93b0f60ae266c997601
Andres Camacho <99270915+af01camacho@users.noreply.github.com>
2023-07-30 15:44:01 -0500

    Update searchdomain.jsx

--- src/components/searchdomain/searchdomain.jsx
+++ src/components/searchdomain/searchdomain.jsx
@@ -1,3 +1,6 @@
+import { faEarthAmericas, faGlobe, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export function SearchDomainDesktop() {
   const info = [
     {
@@ -24,27 +27,53 @@ export function SearchDomainDesktop() {
     },
   ];
   return (
-    <div className="bg-bg-primary dark:text-white dark:bg-primary-color-dm text-center font-second-font pt-10 pb-56">
+    <div className=" dark:text-white dark:bg-primary-color-dm text-center font-second-font pt-10 pb-56">
       <div>
         <h2 className="text-4xl">¿Que te ofrecemos?</h2>
       </div>
-      <div className=" grid   grid-cols-3  place-items-center">
+      <div className=" grid   grid-cols-3 gap-4 p-3 place-items-center">
         {info.map((infos, index) => (
           <div className="" key={index}>
-            <div className="bg-white shadow-2xl w-[352px] dark:bg-gray-600 dark:border-none p-5 flex flex-col justify-between border rounded-md">
+            <div className="bg-white shadow-2xl  dark:bg-gray-600 dark:border-none p-5 flex flex-col justify-between border rounded-md">
               <h2 className="bg-btn-color rounded-lg p-3 text-3xl font-bold uppercase text-white">
                 {infos.title}
               </h2>
               <h3 className="text-xl font-light">{infos.desc}</h3>
               <h2 className="text-2xl pt-10">Planes desde</h2>
               <h4 className="text-4xl font-light pb-10">{infos.price}</h4>
-              <span className="font-semibold dark:text-span-color text-sm p-2">{infos.span}</span>
+              <span className="font-semibold dark:text-span-color text-sm p-2">
+                {infos.span}
+              </span>
               <button className="bg-btn-color p-2 text-xl text-white">
                 Contratar
               </button>
             </div>
           </div>
         ))}
+      </div>
+      <div className="p-4 bg-bg-primary dark:bg-gray-700">
+        <h2 className="text-4xl">Adquiere tu Hosting web fácil</h2>
+        <div className="grid grid-cols-3 gap-4 p-5">
+          <div className="shadow-md rounded-xl  bg-white flex flex-col gap-2 pt-10 items-center dark:bg-gray-600">
+            <div className="bg-primary-color-dm  h-[80px] w-[80px] rounded-full  flex items-center justify-center text-white">
+              <FontAwesomeIcon size="2xl" icon={faGlobe} />
+            </div>
+            <h2 className="text-2xl font-semibold">Registra tus dominios</h2>
+          </div>
+          <div className="shadow-md rounded-xl  bg-white flex flex-col gap-2 pt-10 items-center dark:bg-gray-600">
+            <div className="bg-primary-color-dm  h-[80px] w-[80px] rounded-full  flex items-center justify-center text-white">
+            <FontAwesomeIcon size="2xl" icon={faPaperPlane} />
+            </div>
+            <h2 className="text-2xl font-semibold">Publica tu sitio web</h2>
+          </div>
+          <div className="h-80 shadow-md rounded-xl  bg-white flex flex-col gap-2 pt-10 items-center dark:bg-gray-600">
+            <div className="bg-primary-color-dm  h-[80px] w-[80px] rounded-full  flex items-center justify-center text-white">
+            <FontAwesomeIcon size="2xl" icon={faEarthAmericas} />
+            </div>
+            <h2 className="text-2xl font-semibold">Hazlo visite al mundo</h2>
+          </div>
+          
+        </div>
       </div>
     </div>
   );
