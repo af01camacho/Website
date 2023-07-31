e53009a343efaaac92689d25ed15276c3d5ea130
Gabriel Martínez <martinegabriel47@gmail.com>
2023-07-31 13:21:33 -0500

    update new version

--- src/components/footer/footerDesktop.jsx
+++ src/components/footer/footerDesktop.jsx
@@ -8,8 +8,8 @@ const FooterDesktop = () => {
         {link: "Portafolio", route: "/"}
     ]
     return(
-        <footer className="flex flex-col justify-center h-[150px] p-8 dark:bg-primary-color-dm">
-            <div className="flex py-8 px-4 justify-around">
+        <footer className="flex flex-col justify-center h-[180px] p-8 dark:bg-primary-color-dm">
+            <div className="flex py-8 px-4 justify-around items-center">
                 <div className="dark:text-white">
                     <h1>LOGO</h1>
                 </div>
@@ -26,24 +26,24 @@ const FooterDesktop = () => {
                 </div>
 
                 <div className="dark:text-white">
-                    <input type="text" placeholder="Your Email" />
-                    <button>Suscribe</button>
+                    <input type="text" className="py-2" placeholder="Your Email" />
+                    <button className="px-4 py-2 bg-blue-500 text-white">Suscribe</button>
                 </div>
             </div>
 
-            <div className="flex flex-col dark:text-white">
+            <div className="flex flex-col dark:text-white mb-8">
                 <div className="mb-8">
                     <div className="h-[1px] flex-grow bg-gray-500 mx-10"></div>
                 </div>
 
-                <div className="flex justify-between mx-14">
+                <div className="flex justify-between mx-14 items-center">
                     <div>
-                        <h1>© 2023 Tech Masters Web, All rights reserved.</h1>
+                        <p className="text-gray-500 text-sm">© 2023 Tech Masters Web, All rights reserved.</p>
                     </div>
                     <div className="flex gap-6">
-                        <p>facebook</p>
-                        <p>facebook</p>
-                        <p>facebook</p>
+                        <p className="text-gray-500 text-sm">Facebook</p>
+                        <p className="text-gray-500 text-sm">Instagram</p>
+                        <p className="text-gray-500 text-sm">Github</p>
                     </div>
                 </div>
             </div>
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,26 +2,39 @@ import { useEffect } from "react";
 import { HeroDesktop, HeroMobile } from "../../components/hero/hero";
 import Navbar from "../../components/navbar/navbar";
 import { SearchDomainDesktop } from "../../components/searchdomain/searchdomain";
+import FooterDesktop from "../../components/footer/footerDesktop";
+import FooterMobile from "../../components/footer/footerMobile";
+import FooterTablet from "../../components/footer/footerTablet";
 
 
 function Home({ changeDarkMode }) {
   return (
     <div className="font-primary-font">
-      
       <Navbar changeDarkMode={changeDarkMode} />
       <HeroDesktop />
       {/* Design Mobile */}
       <div className="contents md:hidden lg:hidden sm:hidden xl:hidden font-primary-font">
         {/* <HeroMobile /> */}
-        
-      
       </div>
       {/* Design Desktop */}
       <div className="hidden lg:contents xl:contents">
-        
-
       </div>
-      <SearchDomainDesktop/>
+
+      <div>
+        <SearchDomainDesktop/>
+      </div>
+
+      <div className="hidden lg:contents xl:contents">
+        <FooterDesktop />
+      </div>
+
+      <div className="xl:hidden sm:hidden lg:hidden md:hidden">
+        <FooterMobile />
+      </div>
+
+      <div className="hidden sm:contents md:contents lg:hidden xl:hidden">
+        <FooterTablet />
+      </div>
     </div>
   );
 }
